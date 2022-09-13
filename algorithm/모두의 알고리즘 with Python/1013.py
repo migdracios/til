3714508eea41efebfdb794865d5588021576b609
migdracios <migdracios@gmail.com>
2022-09-13 23:38:49 +0900

    WIP 하노이탑 알고리즘

--- algorithm/모두의 알고리즘 with Python/1013.py
+++ algorithm/모두의 알고리즘 with Python/1013.py
@@ -6,30 +6,20 @@
 출력 : 원반을 옮기는 순서
 '''
 
+# 정답
 def hanoi(n, from_pos, to_pos, aux_pos):
-    # 원반이 한 개일 경우 바로 종료
     if n == 1:
         print(from_pos, "->", to_pos)
         return
-    
-    # 원반이 한 개가 아닐 경우,
-    # n-1개를 중간목적지인 aux_pos로 이동, to_pos를 보조로 사용
+
+    # n이 1이 아니라면, 여기로 내려옴    
     hanoi(n-1, from_pos, aux_pos, to_pos)
+    # 함수를 반복 실행해서 return을 했을 때, 여기로 내려옴
     print(from_pos, "->", to_pos)
+    # 나머지 원반을 목적지로 옮기는 재귀
     hanoi(n-1, aux_pos, to_pos, from_pos)
     
 
 
 
-def hanoi2(disk, start_post, end_post, support_post):
-    # 원반 한 개
-    if disk == 1:
-        print(f"{start_post}-->{end_post}")
-        return
-    # 원반 두 개 이상
-    # 마지막 원반 전까지 support_post로 이동, end_post가 보조 기둥으로 활용
-    hanoi2(disk-1, start_post, support_post, end_post)
-    # 마지막 원반을 end_post로 이동
-    print(print(f"{start_post}-->{end_post}"))
     
-hanoi2(3,1,3,2)
