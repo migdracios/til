60e9fb612fddc5b05ef03cdc63db4b2553e1c1b5
migdracios <migdracios@gmail.com>
2022-09-15 13:59:51 +0900

    Add 평균 비율 출력 알고리즘

--- algorithm/baekjoon/4344.py
+++ algorithm/baekjoon/4344.py
@@ -26,23 +26,50 @@
 55.556%
 '''
 
-def find_avg_per(testcase_num, testcase_list):
-    '''
-    1. 숫자를 합한 뒤, 평균을 구한다
-    2. 평균을 넘은 케이스를 확인하여 카운트한다
-    3. 카운트 / 입력케이스를 나누어 3자리수로 리턴한다
-    '''
-    average_num = sum(*testcase_list) / testcase_num
-    count_num = 0
-    for testcase in testcase_list:
-        if testcase > average_num:
-            count_num += 1
-    avg_per = round(count_num / testcase_num, 3)
-    print(f"{avg_per}%")
-    return
+'''
+알고리즘 로직 설계
 
+1. 테스트케이스의 수를 입력받음
+2. 테스트케이스를 모두 담을 빈 리스트 선언
+3. 테스트케이스 번호를 인자로 담아 저장함수 호출
+4. 입력 받은 테스트케이스 수만큼 저장 함수를 재귀호출하여 리스트 저장
+5. 리스트를 인자로 담아 출력함수를 호출하고 하나씩 pop하면서 재귀호출해 결과를 프린트
+'''
+
+# 테스트케이스 리스트 저장 함수
+def save_score_list(testcase_num, testcase_list):
+    '''
+    1. 테스트케이스 수, 빈 리스트를 인자로 받음
+    2. 테스트케이스의 수 만큼 반복해서 리스트에 어펜딩해야하므로 테스트케이스 수가 0이 되면 리스트를 리턴, 아니면 재개
+    3. 인자로 받은 리스트에 테스트케이스를 입력받아 어펜딩
+    4. 테스트케이스 수를 1 감소시키고 데이터가 들어간 리스트를 담아 재귀호출
+    '''
+    if testcase_num == 0 : return testcase_list
+    testcase_list.append(list(map(int, input().split())))
+    return save_score_list(testcase_num-1, testcase_list)   
+
+
+# 테스트케이스 평균 비율 출력 함수
+def find_avg_per(testcase_list):
+    '''
+    1. 테스트케이스가 모두 담긴 리스트만 입력 받음
+    2. 테스트케이스의 0번째 리스트를 호출해 리스트(처음부터 0번째 인덱스까지)의 합 / 0번째 인덱스로 평균값 계산
+    3. 계산한 평균 값을 조건으로 filter하여 평균 값을 넘는 데이터만 선별
+    4. 선별한 데이터 / 0번째 인덱스로 평균 비율 계산
+    5. fstring을 활용하여 3번째 자릿수까지 퍼센테이지로 출력
+    '''
+    if testcase_list == [] : return
+    average_num = sum(testcase_list[0][1:]) / testcase_list[0][0]
+    count_morethan_average = len(list(filter(lambda x: x>average_num, testcase_list[0][1:])))
+    # count_num = 0
+    # for testcase in testcase_list:
+    #     if testcase > average_num:
+    #         count_num += 1
+    average_per = float(count_morethan_average) / float(testcase_list[0][0])
+    print(f"{average_per*100:.3f}%")
+    del testcase_list[0]
+    return find_avg_per(testcase_list)
+    
 testcase_num = int(input())
-testcase_list = [map(int, input().split())]
-testcase_list.pop(0)    
-find_avg_per(testcase_num, testcase_list)
-
+testcase_list = save_score_list(testcase_num, testcase_list=[])
+find_avg_per(testcase_list)
