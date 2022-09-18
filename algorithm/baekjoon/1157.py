d8bd3796f2c16171ec55c79400ad180a28e7b415
migdracios <migdracios@gmail.com>
2022-09-18 23:50:32 +0900

    Add 최빈값 알고리즘

--- algorithm/baekjoon/1157.py
+++ algorithm/baekjoon/1157.py
@@ -6,26 +6,33 @@
 - 입력 : 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
 - 출력 : 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 '''
-
+  
 '''
 알고리즘 설계
-1. 입력 값을 대문자로 변환
-2. 알파벳 길이 26의 리스트 생성
-3. 입력 값을 반복하면서 해당 알파벳의 위치 값에 +1
-4. 리스트 중에 가장 큰 값을 찾아내어 알파벳 대문자로 출력
+1. 문자열을 입력 받음
+2. 입력 받은 문자열을 대문자로 변환(대소문자를 구분하지 않기 때문)
+3. 알파벳의 길이 만큼의 0이 담겨있는 리스트를 선언
+4. for 입력받은 문자열을 반복하면서 ord 메서드를 활용하여 해당 알파벳 숫자 인덱스에 카운트
+5. max 메서드를 사용하여 리스트 내의 최빈 횟수를 변수에 선언
+6. 같은 횟수의 최빈값을 감안하여 빈 리스트 선언
+7. for if 알파벳 리스트를 enumerate로 반복하여 리스트 내부 값이 최빈 횟수와 같다면
+8. 그 인덱스 번호를 최빈값 리스트에 저장
+9. if 리스트의 길이가 2 이상이라면, ?를 출력
+10. 아니라면 리스트 0번째 값을 chr, ord 메서드를 활용하여 알파벳을 대문자로 출력
 '''
 
-target_text = input()
-target_text = target_text.upper()
-alphabet_list = [0] *26
-max_count = 0
-max_alpha = target_text[0]
-for alphabet in target_text:
-    alphabet = ord(alphabet) - ord("A")
-    alphabet_list[alphabet] += 1
-    if alphabet_list[alphabet] > max_count:
-        max_count = alphabet_list[alphabet]
-        max_alpha = alphabet
-print(chr(max_alpha+ord("A")))
+text = input()
+text = text.upper()
+list_alpha = [0] * 26
 
-
+for i in text:
+    i = ord(i) - ord("A")
+    list_alpha[i] += 1
+    
+maxnum = max(list_alpha)
+maxnum_list = []
+for i,count in enumerate(list_alpha):
+    if count == maxnum:
+        maxnum_list.append(list_alpha.index(count))
+        
+print("?") if len(maxnum_list) >= 2 else print(chr(ord("A")+maxnum_list[0]))
