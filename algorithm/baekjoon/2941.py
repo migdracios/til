b5846171b57dede74b53c9be2aa8d18d2c29d31d
migdracios <migdracios@gmail.com>
2022-09-21 23:07:47 +0900

    WIP 순차탐색 알고리즘

--- algorithm/baekjoon/2941.py
+++ algorithm/baekjoon/2941.py
@@ -50,10 +50,8 @@ def num_croatia(text, count):
     2. for : 문자를 in 메서드로 특정 크로아티아 알파벳이 있는지 조회
     3. if : 있다면 해당 문자를 replace로 공백으로 만듦, 바뀐 문자와 count+1을 인자로 재귀호출
     4. if : 문자열에 더 이상 크로아티아 문자가 없어 반복문이 종료된 경우
-    5. 문자열의 공백을 strip으로 제거한 뒤 len 메서드의 길이만큼 카운트에 추가, 카운트 return
-    # '''
-    if text.strip(" ") is None:
-        return count
+    5. 문자열의 공백을 replace으로 제거한 뒤 len 메서드의 길이만큼 카운트에 추가, 카운트 return
+    '''
     for croatia_alphabet in alphabet_list:
         if croatia_alphabet in text:
             return num_croatia(text.replace(croatia_alphabet, " ", 1), count+1)
@@ -70,5 +68,6 @@ print(num_croatia(input_text, count))
 1. 반복문이 끝나도 문자열이 남아있음 -> 반복문이 끝난 이후 문자열의 길이만큼 카운트 해줌
 2. 문자열을 제거했는데 그냥 알파벳+크로아티아 알파벳의 조합으로 크로아티아 알파벳이 되어버린 경우
 -> 제거된 문자열을 공백으로 대체해서 합쳐지는 케이스 해결
-3. 같은 문자열이 여러 개 있는 경우 한 번이 아니라 여러개를 삭제해버림 -> replace 세 번째 인자로 횟수를 1회로 고정
+3. 같은 문자열이 여러 개 있는 경우 한 번이 아니라 여러 개를 삭제해버림 -> replace 세 번째 인자로 횟수를 1회로 고정
+4. 문자열이 더 이상 남아있지 않은 경우 리턴 카운트 실행이 되지 않음 -> 아래쪽 count 더해주기 로직에서 모두 해결 가능하기에 삭제
 '''
